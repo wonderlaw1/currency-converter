f3aaaeacc2339edffb5776f850b5c6d375ce722c
wonderlaw12 <wonderlaw12@gmail.com>
2025-07-04 13:08:18 +0200

    dotenv added

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
 import { App } from './app/app';
+import { appConfig } from './app/app.config';
+// .env init
+import 'dotenv/config';
 
-bootstrapApplication(App, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(App, appConfig).catch((err) => console.error(err));
